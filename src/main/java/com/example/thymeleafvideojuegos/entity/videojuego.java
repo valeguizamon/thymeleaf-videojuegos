7d0bc5b98d5cf4d25843b44cde53b99d4969ef0b
Valentín Leguizamón <valentin.leguizamon.sevilla@gmail.com>
2021-08-06 23:04:39 -0300

    Entidades

---
+++ src/main/java/com/example/thymeleafvideojuegos/entity/videojuego.java
@@ -0,0 +1,46 @@
+package com.example.thymeleafvideojuegos.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@Table(name = "videogames")
+public class videojuego implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String titulo;
+
+    private String descripcion;
+
+    private String imagen;
+
+    private float precio;
+
+    private boolean oferta;
+
+    private int stock;
+
+    @JoinColumn(name = "estudio", nullable = false, updatable = false)
+    @ManyToOne
+    private estudio estudio;
+
+    @JoinColumn(name = "genero", nullable = false, updatable = false)
+    @ManyToOne
+    private genero genero;
+
+    @Temporal(TemporalType.DATE)
+    private Date releaseDate;
+
+    public videojuego() {}
+}
