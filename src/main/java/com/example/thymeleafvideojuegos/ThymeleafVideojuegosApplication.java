84337a4db6b1ad9f36fd8ec62dc1a1d935dddc89
Valentín Leguizamón <valentin.leguizamon.sevilla@gmail.com>
2021-08-06 22:09:19 -0300

    Commit Inicial, se crea el proyecto con sus dependencias.

---
+++ src/main/java/com/example/thymeleafvideojuegos/ThymeleafVideojuegosApplication.java
@@ -0,0 +1,13 @@
+package com.example.thymeleafvideojuegos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafVideojuegosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafVideojuegosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/thymeleafvideojuegos/ThymeleafVideojuegosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thymeleafvideojuegos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafVideojuegosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
